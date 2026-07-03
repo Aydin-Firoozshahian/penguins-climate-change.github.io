ff2617e339cc4f52cd52876c8ec182c3c3ba1843
Aydin-Firoozshahian <aydin26f@gmail.com>
2026-07-03 15:05:39 -0700

    update d3 to v6, make script for graph

---
+++ src/pg1-viz.js
@@ -0,0 +1,6 @@
+// Set the margin and dimensions
+var margin = {top: 10, right: 30, bottom: 30, left: 60}
+    width = 460 - margin.left - margin.right
+    height = 400 - margin.top - margin.bottom;
+
+// put the svg object for the visualization in the div container in the html
